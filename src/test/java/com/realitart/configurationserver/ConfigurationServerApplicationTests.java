1e2978ef766b3ca26629a8918c12d89b9a076a70
eduperaltas <eduperaltas@gmail.com>
2023-08-06 19:30:11 -0500

    first commit

---
+++ src/test/java/com/realitart/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realitart.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
